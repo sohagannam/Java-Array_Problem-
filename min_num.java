9b0627169d92fb89693d3fbef4801ba822b07b82
sohagannam <90404157+sohagannam@users.noreply.github.com>
2022-02-02 14:17:18 -0700

    Add files via upload

---
+++ min_num.java
@@ -0,0 +1,12 @@
+public class min_num {
+    public static void main(String[] args) {
+        int myarray[]={2,3,4,5,6,7};
+        int max=myarray[0];
+        for(int i=0;i<myarray.length;i++){
+            if(myarray[i]<max){
+                max=myarray[i];
+            }
+        }
+      System.out.print(max+" ");
+    }
+}
